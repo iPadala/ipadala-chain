a6501db859f9ccdf43be310a54a30e32025e4680
sugoiporu <sugoiporu@gmail.com>
2021-01-24 09:11:51 +0800

    added api endpoints

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 
-const { Blockchain } = require('./lib/blockchain')
+const { Block, Blockchain } = require('./lib/blockchain')
+const Transaction = require('./lib/transaction')
 const TransactionPool = require('./lib/transactionPool')
 const Wallet = require('./lib/wallet')
 const P2pServer = require('./lib/p2p')
+const Minter = require('./lib/minter')
 const Util = require('./lib/util')
+const catchAsync = require('./lib/catchAsync')
 
 const blockchain = new Blockchain()
 const transactionPool = new TransactionPool()
@@ -18,28 +21,121 @@ const initHttpServer = (port) => {
     const app = express()
     app.use(bodyParser.json())
 
+    // GET
+    app.get('/blocks', catchAsync((req, res) => {
+        res.send(blockchain._chain)
+    }))
+
+    app.get('/blocks/latest', catchAsync((req, res) => {
+        res.send(blockchain.getLatestBlock())
+    }))
+
+    app.get('/blocks/:id', catchAsync((req, res) => {
+        res.send(blockchain.getBlockByHash(req.params.id))
+    }))
+
+    app.get('/transactions', catchAsync((req, res) => {
+        return res.json(transactionPool.validTransactions())
+    }))
+
+    app.get('/transaction/:id', catchAsync((req, res) => {
+        const inBlock = blockchain.getTransactionByHash(req.params.id)
+        const inPool = transactionPool.getTransactionByHash(req.params.id)
+        let tx = { error: 'Transaction not found.' }
+        if (Object.entries(inBlock).length > 0) {
+            inBlock.confirmation = blockchain.getLatestBlock().height - blockchain.getBlockByHeight(inBlock.block).height
+            tx = inBlock
+        }
+        if (Object.entries(inPool).length > 0) {
+            inPool.confirmation = 0
+            tx = inPool
+        }
+        return res.json(tx)
+    }))
+
+    app.get('/address/:id', catchAsync((req, res) => {
+        const balanceInfo = wallet.getBalanceInfo(req.params.id)
+        res.send({
+            balance: balanceInfo.received - balanceInfo.sent,
+            received: balanceInfo.received,
+            sent: balanceInfo.sent,
+            transactions: blockchain.getTransactionsByAddress(req.params.id)
+        })
+    }))
+
+    app.get('/createWallet', catchAsync((req, res) => {
+        res.send(wallet.createWallet())
+    }))
+
+    app.get('/walletBalance', catchAsync((req, res) => {
+        res.send({ balance: wallet.getBalance(wallet._wallet.address) })
+    }))
+
+    app.get('/walletInfo', catchAsync((req, res) => {
+        res.send(wallet.getWallet())
+    }))
+
+    // POST
+
+    app.post('/transaction', catchAsync((req, res) => {
+        const { address, amount, secret } = req.body
+        if (!address) throw Error('Missing receiving address')
+        if (!amount) throw Error('Missing amount')
+        let txWallet = wallet.getWallet()
+        if (secret) txWallet = Wallet.getWalletInfoBySecret(secret)
+        if (txWallet === null) throw Error('No wallet specified')
+        if (transactionPool.hasExistingTransaction(txWallet.address)) throw Error('Double spend detected')
+        const tx = Transaction.createTransaction(txWallet, address, amount)
+        if (!Transaction.verifyTransaction(tx)) throw Error('Invalid transaction ' + tx)
+        transactionPool.updateOrAddTransaction(tx)
+        p2p.broadcast(Util.events().NEW_TRANSACTION, tx)
+        res.send(tx)
+    }))
+
+    app.post('/mint', catchAsync((req, res) => {
+        const { address } = req.body
+        if (!address) throw Error('Missing receiving address')
+        const minter = new Minter(Block, blockchain, transactionPool, { address, balance: wallet.getBalance(address) }, p2p)
+        const block = minter.mint()
+        if (!block) throw Error('No blocks minted')
+        p2p.broadcast(Util.events().NEW_BLOCK, block)
+        res.send(block)
+    }))
+
+    app.post('/peers', catchAsync((req, res) => {
+        const { host, port } = req.body
+        if (!host) throw Error('Missing host')
+        if (!port) throw Error('Missing port')
+        p2p.connectToPeer(`http://${host}:${port}`)
+        res.status(201).end()
+    }))
+
     app.use((err, req, res, next) => {
         if (err) {
-            res.status(400).send(err.message)
+            res.status(400).send({ error: err.message })
         }
     })
 
-    app.get('/blocks', (req, res) => {
-        res.send(blockchain._chain)
-    })
+    // PUT
 
-    app.get('/createWallet', (req, res) => {
-        res.send(wallet.createWallet())
-    })
-
-    app.get('/walletBalance', (req, res) => {
-        res.send({ balance: wallet.getBalance(wallet._wallet.address) })
+    app.put('/setWallet', (req, res) => {
+        const { secret } = req.body
+        if (!secret) throw Error('Missing secret')
+        wallet.setWallet(Wallet.getWalletInfoBySecret(secret))
+        res.status(201).end()
     })
 
     app.listen(port, () => {
         console.log('Listening http on port: ' + port)
     })
 }
-if (config.BLOCK_WALLET !== null) wallet.setWallet(Util.readFile(config.BLOCK_WALLET))
+if (config.BLOCK_WALLET !== null) {
+    Util.readFile(config.BLOCK_WALLET, (err, data) => {
+        if (err) throw err
+        wallet.setWallet(data)
+        console.log('Wallet file set:', config.BLOCK_WALLET)
+    })
+}
+console.log('Minting enabled:', config.MINTING_ENABLED)
 p2p.init(config.P2P_PORT)
 initHttpServer(config.API_PORT)
--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
+const packageJson = require('../package.json')
+
 const args = {}
 
 process.argv.forEach(function (val) {
@@ -6,13 +8,17 @@ process.argv.forEach(function (val) {
     if (val.startsWith('--a')) args.ap = val.split('=')[1]
     if (val.startsWith('--p')) args.pp = val.split('=')[1]
     if (val.startsWith('--w')) args.bw = val.split('=')[1]
+    if (val.startsWith('--m')) args.me = val.split('=')[1]
 })
 
 module.exports = {
-    BLOCK_GENERATION_INTERVAL: args.bgi || 10,
+    BLOCK_GENERATION_INTERVAL: args.bgi || 20,
     DIFFICULTY_ADJUSTMENT_INTERVAL: args.dai || 10,
     API_PORT: args.ap || 8080,
     P2P_PORT: args.pp || 21121,
     BLOCK_WALLET: args.bw || null,
-    NODE_VERSION: '1.0.0'
+    BLOCK_CONFIRMATION: 3,
+    ALLOW_ZERO_BALANCE_MINTER: true,
+    MINTING_ENABLED: (args.me === 'true' || args.me === true) || false,
+    NODE_VERSION: packageJson.version
 }

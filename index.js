3d3157bda02a4c76faecc59311165d131ec4a467
sugoiporu <sugoiporu@gmail.com>
2021-01-26 07:47:15 +0800

    updated package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+module.exports.Util = require('./dist/util')
+module.exports.Block = require('./dist/block')
+module.exports.Blockchain = require('./dist/blockchain')
+module.exports.Transaction = require('./dist/transaction')
+module.exports.TransactionPool = require('./dist/transactionPool')
+module.exports.Wallet = require('./dist/wallet')
+module.exports.P2pServer = require('./dist/p2pServer')
+module.exports.Minter = require('./dist/minter')
